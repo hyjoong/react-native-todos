53e55e0fc477862e6a44778840e2252a78e90894
hyjoong <maplesosser@naver.com>
2021-07-30 20:03:01 +0900

    feat: main page layout

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput, View, Text } from "react-native";
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World</Text>
+      <Text style={styles.appTitle}>Todolist</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder="input todo" />
+      </View>
     </SafeAreaView>
   );
 };
@@ -12,9 +15,31 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#3143e8",
+  },
+  appTitle: {
+    color: "#fff",
+    fontSize: 36,
+    marginTop: 30,
+    marginBottom: 30,
+    fontWeight: "300",
+    textAlign: "center",
+    backgroundColor: "#3143e8",
+  },
+  card: {
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    flex: 1,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 24,
+    marginLeft: 20,
   },
 });
 
